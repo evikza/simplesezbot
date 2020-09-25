2cf2560cbe0243021be73e152987ff0caf0b6946
evikza <48568977+evikza@users.noreply.github.com>
2020-09-26 02:55:59 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+$botToken = "1167269062:AAHX7Nvh3SkhnRmEf-69IPN_DAN-9d1D5o0"; 
+
+$website = "https://api.telegram.org/bot" . $botToken; 
+
+// $update = file_get_contents($website . "/getUpdates"); 
+// $updateArray = json_decode($update, TRUE); 
+
+// $chatId = $updateArray["result"][0]["message"]["chat"]["id"]; 
+
+file_get_contents($website . "/sendMessage?chat_id=179897095&text=" . uniqid('', 0));
