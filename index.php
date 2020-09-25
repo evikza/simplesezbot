0b65e458a674debc3f046ba551ee7245b171c640
evikza <48568977+evikza@users.noreply.github.com>
2020-09-26 03:44:41 +0500

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ $botToken = "1167269062:AAHX7Nvh3SkhnRmEf-69IPN_DAN-9d1D5o0";
 
 $website = "https://api.telegram.org/bot" . $botToken; 
 
-$update = file_get_contents($website . "/getUpdates"); 
-$updateArray = json_decode($update, TRUE); 
+$update = file_get_contents('php://input');
+$update = json_decode($update, TRUE);
 
-$chatId = $updateArray["result"][0]["message"]["chat"]["id"]; 
+$chatId = $update["result"][0]["message"]["chat"]["id"]; 
 
-file_get_contents($website . "/sendMessage?chat_id=" . $chatId . "&text=" . uniqid('', true));
+file_get_contents("https://api.telegram.org/bot1167269062:AAHX7Nvh3SkhnRmEf-69IPN_DAN-9d1D5o0/sendMessage?chat_id=". $chatId ."&text=dfgdfgdfggdfgdfg");
